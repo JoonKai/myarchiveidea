484d074a77813d0f7cbec695670d0ea277237139
JoonKai <joonseok831@gmail.com>
2024-01-01 16:46:16 +0900

    11111

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:myarchiveidea/Screen/main_screen.dart';
 import 'package:myarchiveidea/Screen/splash_screen.dart';
+import 'package:myarchiveidea/data/idea_info.dart';
+
+import 'Screen/edit_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -18,6 +21,20 @@ class MainApp extends StatelessWidget {
       routes: {
         '/': (context) => const SplashScreen(),
         '/main': (context) => const MainScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/edit') {
+          //1.아이디어 기록값을 넘기지 못한다면 작성 시나리오
+
+          //2.아이디어 기록값을 넘길 수 있다면 수정 시나리오
+          final IdeaInfo? ideaInfo = settings.arguments as IdeaInfo?;
+          return MaterialPageRoute(
+            builder: (context) {
+              return EditScreen();
+            },
+          );
+        }
+        return null;
       },
     );
   }
